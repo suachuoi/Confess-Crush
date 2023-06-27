285dad7988b2d875ced507c76ec8daa655095c3f
suachuoi <137899385+suachuoi@users.noreply.github.com>
2023-06-27 20:14:55 +0700

    Update config.jshttps://www.messenger.com/t/100024960013449

--- config.js
+++ config.js
@@ -1,20 +1,15 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "Hi, Mỹ Tâm",
+    introTitle: 'Đôi lời nhắn nhủ!!!',
+    introDesc: `Ngoài những thứ cần thiết như viết, viết chì, tẩy, thước, QUAN TRỌNG là CCCD, GIẤY DỰ THI và MỘT TINH THẦN THẬT THOẢI MÁI KHÔNG BUỒN NGỦ`,
+    btnIntro: 'Rồii',
+    title: 'Có biết e làm gì tiếp theo hok',
+    desc: 'đoán điii ',
+    btnYes: 'Sao tao phải đoán?',
+    btnNo: 'Đang đoán',
+    question: 'Em biết là 2 cũng lo, cũng sợ vì dù sao đây cũng là kì thi cực kì quan trọng đối với bất kì ai, nhma 2 đừng có lo lắng quá nheee, e tin là 2 sẽ luôn làm những thứ 2 cho là tốt nhất với bản thân mình, mong 2 sẽ giữ tâm trạng thật tốt cho 2 ngày này, btw chúc cho mục tiêu 26+ của 2 sẽ đến thật dễ dàng và xứng đáng! <3',
+    btnReply: 'Em yêu 2 nhiềuuu  <3',
+    reply: '2 cũng yêu e, mav dth nhất kekeke',
+    mess: 'Em biết mà 🥰.Đừng có cảm động quá nka',
+    messLink: 'https://www.messenger.com/t/100024960013449' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
